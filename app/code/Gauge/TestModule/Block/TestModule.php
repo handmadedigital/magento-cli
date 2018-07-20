3bd18cb7f2574b0ebb06181dc44865cf3fba251f
Carlos Guevara <carlos@handmade-digital.com>
2018-07-20 12:55:59 -0400

    fixed route ID and block

--- app/code/Gauge/TestModule/Block/TestModule.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Gauge\TestModule\Block;
-
-class TestModule extends \Magento\Framework\View\Element\Template implements
-    \Magento\Framework\DataObject\IdentityInterface
-{
-   
-}
